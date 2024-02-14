a310a72bfb85b183db59c615128d5728999010b3
rajatjog <rajat.jog@mindbowser.com>
2024-02-14 09:47:59 +0530

    Create Simple Custom Pagination in Django REST Framework

--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from books.models import Book
+from .paginations import CustomPagination
 
 from books.serializers import BookSerializer
 
@@ -11,6 +12,7 @@ class BookListAPI(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = (AllowAny,)
     authentication_classes = (JWTAuthentication,)
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         return Book.objects.all()
@@ -24,4 +26,4 @@ class BookListAPI(generics.ListAPIView):
             return self.get_paginated_response(serializer.data)
 
         serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
